775ad02c5e3e84a4b5eccb0afee190648aac9e39
shajugit <v.shajahan1996@gmail.com>
2026-06-02 22:36:49 +0530

    shell

--- install.sh
+++ install.sh
@@ -3,10 +3,17 @@
 dnf list installed mysql
 
 if [ $? -eq 0 ]; then
-    echo "mysql is alredy installed"
+    echo "mysql is alredy installed skipping installation"
 else
     echo "mysql is not installed, installing now"
     dnf install mysql-server -y
-    systemctl start mysqld
-    systemctl enable mysqld
+    if [ $? -eq 0 ]; then
+        echo "mysql is installed"
+    else
+        echo "mysql is failed to install"
+        exit 1
+        fi
 fi
+#     systemctl start mysqld
+#     systemctl enable mysqld
+# fi
