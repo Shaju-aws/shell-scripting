15af4321f0d498c8a3cd54eee50eddc254780823
shajugit <v.shajahan1996@gmail.com>
2026-06-05 10:15:14 +0530

    shell

--- roboshop.sh
+++ roboshop.sh
@@ -18,5 +18,24 @@ if [ $# -lt 2 ]; then
     exit 1
 fi
 
+ACTION=$1
+shift
 
+if [ "$ACTION" != "create" ] && [ "$ACTION" != "delete" ]; then
+    echo "Error: Invalid action. Use 'create' or 'delete'."
+    echo "Usage: $0 [create/delete] [instance1 instance2 ...]"
+    exit 1
+fi
+
+get_instanceID() {
+    aws ec2 describe-instances --filters "Name=tag:Name,Values=Roboshop-$name" "Name=instance-state-name,Values=running" \
+    --query "Reservations[0].Instances[0].InstanceId" --output text
+}
+
+# aws ec2 run-instances \
+#    --image-id $AMI_ID \ # Replace with your desired AMI ID
+#    --count 1 \
+#    --instance-type t2.micro \
+#    --security-groups MySecurityGroup \
+#    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=MyEC2Instance}]'
 
