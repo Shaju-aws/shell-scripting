ee018fe1f49ed28c82f10009ad8cca0b717cd014
shajugit <v.shajahan1996@gmail.com>
2026-06-05 10:23:48 +0530

    shell

--- roboshop.sh
+++ roboshop.sh
@@ -28,14 +28,25 @@ if [ "$ACTION" != "create" ] && [ "$ACTION" != "delete" ]; then
 fi
 
 get_instanceID() {
+    name=$1
     aws ec2 describe-instances --filters "Name=tag:Name,Values=Roboshop-$name" "Name=instance-state-name,Values=running" \
     --query "Reservations[0].Instances[0].InstanceId" --output text
 }
 
-# aws ec2 run-instances \
-#    --image-id $AMI_ID \ # Replace with your desired AMI ID
-#    --count 1 \
-#    --instance-type t2.micro \
-#    --security-groups MySecurityGroup \
-#    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=MyEC2Instance}]'
-
+for instance in $@
+do
+INSTANCE_ID=$(get_instanceID $instance)
+if [ "$ACTION" == "create" ]; then
+    if [ "$INSTANCE_ID" == "None" ]; then
+        echo "creating instance for Roboshop-$instance"
+        INSTANCE_ID=$(aws ec2 run-instances \
+                    --image-id $AMI_ID \ # Replace with your desired AMI ID
+                    --count 1 \
+                    --instance-type t2.micro \
+                    --security-groups Roboshop-common Roboshop-$instance \
+                    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Roboshop-'$instance'}]' \
+                    --query "Instances[0].InstanceId" --output text)
+        echo "Instance created with ID: $INSTANCE_ID"
+        else
+        echo "Instance for Roboshop-$instance already exists with ID: $INSTANCE_ID"
+    fi
